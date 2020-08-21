159f75cb792fb462311e16c8cbddbe6044311b77
Joe Averbukh <joeaverbukh@gmail.com>
2020-08-21 16:59:30 -0400

    Finish chapter 1 exercises

--- src/chp1.clj
+++ src/chp1.clj
@@ -71,3 +71,47 @@
     1 [[1]]
     (let [prev-p (pascal (- n 1))]
       (conj prev-p (next-row (last prev-p))))))
+
+;; Exercise 1.16
+(defn fast-exp [b n]
+  (loop [a 1
+         b b
+         n n]
+    (cond
+      (= 0 n) a
+      (odd? n) (recur (* a b) b (- n 1))
+      :else (recur a (* b b) (/ n 2)))))
+
+;; Exercise 1.41
+(defn double [f]
+  (fn [a] ( f (f a))))
+
+;; Exercise 1.42
+(defn compose [& fs]
+  (fn [a] (->> fs
+               reverse
+               (reduce (fn [res f] (f res))
+                       a))))
+
+(defn square [x] (* x x))
+
+;; Exercise 1.43
+(defn repeated-loop [f n]
+  (loop [n n
+         f f]
+    (if (<= n 1)
+      f
+      (recur (- n 1) (compose f f)))))
+
+(defn repeated-range [f n]
+  (->> (range n)
+       (map (fn [_] f))
+       (apply compose)))
+
+;; Exercise 1.46
+(defn iterative-improve [good-enough? improver]
+  (fn [guess]
+    (loop [guess guess]
+      (if (good-enough? guess)
+        guess
+        (recur (improver guess))))))
