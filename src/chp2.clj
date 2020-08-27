c4f2a002fc40314c6e135abf1f6bf86b4dcb99b5
Joe Averbukh <joeaverbukh@gmail.com>
2020-08-27 15:31:34 -0400

    More chapter 2

--- src/chp2.clj
+++ src/chp2.clj
@@ -148,14 +148,119 @@
                    (range board-size))))
        (run! println)))
 
-; 2.53
-; 2.55
 ; 2.59
-; 2.60
+(defn adjoin-set [s e]
+  (if (some #{e} s)
+    s
+    (conj s e)))
+
+(defn union-set [s1 s2]
+  (reduce adjoin-set s1 s2))
+
 ; 2.61
+(defn adjoin-set-ordered [s e]
+  (cond
+    (or (not (seq s)) (< e (first s)))
+    (cons e s)
+
+    (= e (first s))
+    s
+    :else
+    (cons (first s) (adjoin-set-ordered (rest s) e))))
+
 ; 2.62
+(defn union-set-ordered [s1 s2]
+  (let [a (first s1)
+        b (first s2)]
+    (cond
+      (nil? a)
+      s2
+
+      (nil? b)
+      s1
+
+      (< a b)
+      (cons a (union-set-ordered (rest s1) s2))
+
+      (> a b)
+      (cons b (union-set-ordered s1 (rest s2)))
+
+      :else
+      (cons a (union-set-ordered (rest s1) (rest s2))))))
+
 ; 2.67
+(defn left-branch [tree]  (first tree))
+(defn right-branch [tree] (second tree))
+(defn symbols [tree] (tree 2))
+(defn weight [tree] (tree 3))
+(defn make-leaf [symbol weight] [nil nil [symbol] weight])
+(defn leaf? [tree] (and
+                     (not (left-branch tree))
+                     (not (right-branch tree))))
+
+(defn make-code-tree [left right]
+  [left
+   right
+   (concat (symbols left) (symbols right))
+   (+ (weight left) (weight right))])
+
+(defn decode [tree bits]
+  (letfn [(choose-branch [bit branch]
+            ((if (= bit 0) left-branch right-branch)
+             branch))
+          (decode-helper [bits current-branch]
+            (if (not (seq bits))
+              '()
+              (let [next-branch (choose-branch (first bits) current-branch)]
+                (if (leaf? next-branch)
+                  (concat (symbols next-branch) (decode-helper (rest bits) tree))
+                  (decode-helper (rest bits) next-branch)))))]
+    (decode-helper bits tree)))
+
+(comment
+  (def sample-tree (make-code-tree (make-leaf 'A 4)
+                                   (make-code-tree (make-leaf 'B 2) (make-code-tree
+                                                                      (make-leaf 'D 1)
+                                                                      (make-leaf 'C 1)))))
+  (def sample-bits '(0 1 1 0 0 1 0 1 0 1 1 1 0))
+  (def sample-message '(A D A B B C A))
+  (= (decode sample-tree sample-bits) sample-message))
+
 ; 2.68
+(defn encode [tree message]
+  (letfn [(encode-symbol-helper [symbol path branch]
+            (cond
+              (= [symbol] (symbols branch))
+              path
+
+              (leaf? branch)
+              nil
+              :else
+              (let [left (left-branch branch)
+                    right (right-branch branch)]
+                (or
+                  (and left (encode-symbol-helper symbol (conj path 0) left))
+                  (and right (encode-symbol-helper symbol (conj path 1) right))))))
+          (encode-symbol [symbol]
+            (let [res (encode-symbol-helper symbol [] tree)]
+              (if res
+                res
+                (throw (Exception. (format "Uh oh, couldn't find symbol = %s" symbol))))))
+          (encode-helper [message res]
+            (if (not (seq message))
+              res
+              (encode-helper (rest message) (concat res (encode-symbol (first message))))))]
+    (encode-helper message [])))
+
+(comment
+  (def sample-tree (make-code-tree (make-leaf 'A 4)
+                                   (make-code-tree (make-leaf 'B 2) (make-code-tree
+                                                                      (make-leaf 'D 1)
+                                                                      (make-leaf 'C 1)))))
+  (def sample-bits '(0 1 1 0 0 1 0 1 0 1 1 1 0))
+  (def sample-message '(A D A B B C A))
+  (= (encode sample-tree sample-message) sample-bits))
+
 ; 2.69
 ; 2.73 abd
 ; 2.74
