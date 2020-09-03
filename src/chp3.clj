8ac7776d8c269e7dd0ec2ca9ed405db4ba84395a
Joe Averbukh <joeaverbukh@gmail.com>
2020-09-03 17:55:52 -0400

    More chapter 3

--- src/chp3.clj
+++ src/chp3.clj
@@ -87,6 +87,7 @@
     true
     (nil? (rest pair))
     false
+    :else
     (cyclical-iter?
       (conj seen pair)
       (rest pair))))
@@ -95,3 +96,86 @@
     (cyclical-iter? seen pair)))
 
 ; 3.28
+(declare after-delay set-signal! or-gate-delay get-signal add-action!)
+(defn logical-or [a b]
+  (if (or (= a 1) (= b 1))
+    1 0))
+(defn or-gate [a1 a2 output]
+  (letfn [(or-action []
+            (after-delay
+              or-gate-delay
+              #(set-signal! output (logical-or (get-signal a1) (get-signal a2)))))]
+    (add-action! a1 or-action)
+    (add-action! a2 or-action)))
+
+
+; 3.47
+(defn semaphore [n]
+  (let [cnt (atom 0)]
+    (letfn [(test-and-set! []
+              (if (>= @cnt n)
+                true
+                (do
+                  (swap! cnt inc)
+                  false)))
+            (the-semaphore [action]
+              (loop []
+                (condp = action
+                  'acquire
+                  (if (test-and-set!)
+                    (recur)
+                    'ok)
+                  'release
+                  (swap! cnt dec))))]
+      the-semaphore)))
+
+(defmacro cons-stream [a b]
+  `[~a (delay ~b)])
+
+(defn numbers-starting-from [n]
+  (cons-stream n (numbers-starting-from (+ 1 n))))
+
+(defn car-stream [s] (first s))
+(defn cdr-stream [s]
+  @(second s))
+
+(defn ref-stream [s n]
+  (if (= n 0)
+    (car-stream s)
+    (ref-stream (cdr-stream s) (- n 1))))
+
+; 3.50
+(defn map-stream [f & streams]
+  (if (nil? (first streams))
+    '()
+    (cons-stream
+      (apply f (map car-stream streams))
+      (apply map-stream (cons f (map cdr-stream streams))))))
+
+(comment
+  (def numbers (numbers-starting-from 1))
+  (def x (map-stream + numbers numbers numbers))
+  (= (ref-stream x 0) 3)
+  (= (ref-stream x 10) 33))
+
+; 3.53
+; 3.54
+; 3.55
+; 3.56
+; 3.58
+; 3.63
+; 3.64
+; 3.65
+; 3.66
+; 3.67
+; 3.68
+; 3.69
+; 3.70
+; 3.71
+; 3.72
+; 3.77
+; 3.78
+; 3.79
+; 3.81
+; 3.82
+
