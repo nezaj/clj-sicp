6ac7f0fd1ce2ce44b244369b8e20022996cc2743
Joe Averbukh <joeaverbukh@gmail.com>
2020-09-04 16:37:10 -0400

    More chapter 3

--- src/chp3.clj
+++ src/chp3.clj
@@ -130,7 +130,7 @@
       the-semaphore)))
 
 (defmacro my-delay [& forms]
-  `(fn [] ~@forms))
+  `(memoize (fn [] ~@forms)))
 
 (defn my-force [f]
   (f))
@@ -150,6 +150,8 @@
     (car-stream s)
     (ref-stream (cdr-stream s) (- n 1))))
 
+(defn nil-stream? [s] (or (not s) (nil? (first s))))
+
 ; 3.50
 (defn map-stream [f & streams]
   (if (nil? (first streams))
@@ -165,23 +167,109 @@
   (= (ref-stream x 10) 33))
 
 ; 3.53
+(defn add-streams [s1 s2]
+  (map-stream + s1 s2))
+
+(def double (cons-stream 1 (add-streams double double)))
+
 ; 3.54
+(defn mul-streams [s1 s2]
+  (map-stream * s1 s2))
+
+(def integers (numbers-starting-from 1))
+(def factorial-stream
+  (cons-stream 1 (mul-streams factorial-stream integers)))
+
 ; 3.55
+(def partial-sums
+  (cons-stream (car-stream integers) (add-streams partial-sums (cdr-stream integers))))
+
 ; 3.56
-; 3.58
-; 3.63
+(defn scale-stream [s factor] (map-stream (partial * factor) s))
+
+(defn merge-stream [s1 s2]
+  (cond
+    (nil-stream? s1) s2
+    (nil-stream? s2) s1
+
+    :else
+    (let [s1-car (car-stream s1)
+          s2-car (car-stream s2)]
+      (cond
+        (< s1-car s2-car) (cons-stream s1-car (merge-stream (cdr-stream s1) s2))
+        (< s2-car s1-car) (cons-stream s2-car (merge-stream (cdr-stream s2) s1))
+        :else
+        (cons-stream s1-car (merge-stream (cdr-stream s1) (cdr-stream s2)))))))
+
+; Numbers that exclusively have prime factors of 2, 3, and/or 5
+(def special-numbers (cons-stream 1 (merge-stream
+                                      (scale-stream special-numbers 2)
+                                      (merge-stream
+                                        (scale-stream special-numbers 3)
+                                        (scale-stream special-numbers 5)))))
+
 ; 3.64
-; 3.65
-; 3.66
+(def half-stream (cons-stream 1
+                              (map-stream #(* 1.0 (/ % 2))
+                                          half-stream)))
+
+(defn stream-limit [s tolerance]
+  (letfn [(iter [previous s]
+            (let [current (car-stream s)]
+              (if (<= (Math/abs (- current previous)) tolerance)
+                current
+                (iter current (cdr-stream s)))))]
+    (iter (car-stream s) (cdr-stream s))))
+
 ; 3.67
+(defn interleave [s1 s2]
+  (cons-stream (car-stream s1) (interleave s2 (cdr-stream s1))))
+
+(defn pairs [s1 s2]
+  (cons-stream
+    (list (car-stream s1) (car-stream s2))
+    (interleave
+      (map-stream
+        (fn [x] (list (car-stream s1) x))
+        (cdr-stream s2))
+      (pairs (cdr-stream s1) (cdr-stream s2)))))
+
+(defn symmetric [s]
+  (let [s-car (car-stream s)
+        reverse-s-car (reverse s-car)
+        next-part (cons-stream
+                    reverse-s-car
+                    (symmetric (cdr-stream s)))]
+    (if (= s-car reverse-s-car)
+      next-part
+      (cons-stream s-car next-part))))
+
+(defn all-pairs [s1 s2]
+  (symmetric (pairs s1 s2)))
+
+(comment
+  (do
+    (def all-pairs-stream (all-pairs integers integers))
+    (->> (range 20)
+         (map (fn [x] [x (ref-stream all-pairs-stream x)])))))
+
 ; 3.68
+
 ; 3.69
+
 ; 3.70
+
 ; 3.71
+
 ; 3.72
+
 ; 3.77
+
 ; 3.78
+
 ; 3.79
+
 ; 3.81
+
 ; 3.82
 
